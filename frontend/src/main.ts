28eddbb0b26900ff2a1cb63e867e9fcf597ad0ee
Oeun Nuphea <nupheaoeun@gmail.com>
2026-03-02 14:40:54 +0700

    udpate

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,7 @@
 import './assets/main.css'
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/700.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
