be7ab473ae873b3c79057edc0f51357f48691570
Oeun Nuphea <nupheaoeun@gmail.com>
2026-03-02 11:53:54 +0700

    add frontend

--- NotionUI/server/api/notes/index.post.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig();
-  const body = await readBody(event);
-
-  return await $fetch(`${config.public.noteServiceBase}/notes`, {
-    method: "POST",
-    body,
-  });
-});
---
+++ frontend/vite.config.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_NOTE_SERVICE_BASE_URL || 'http://localhost:3003',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
