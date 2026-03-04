3605a9860442b3d4436ec7dfa5af7593e4c95114
Oeun Nuphea <nupheaoeun@gmail.com>
2026-03-04 08:33:37 +0700

    update

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -4,9 +4,26 @@ import authProxy from "./routes/auth.proxy";
 import noteProxy from "./routes/note.proxy";
 import publicNoteProxy from "./routes/public-note.proxy";
 import userProxy from "./routes/user.proxy";
+import rateLimit from "express-rate-limit";
+import { error } from "console";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+// If behind nginx/reverse proxy:
+app.set('trust proxy', 1)
+
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {error: "Too many requests, please try again later."}
+})
+
+
+app.use(globalLimiter);
+
 
 // ── Health check (no auth required) ─────────────────────────────────────────
 app.get("/health", (_req: Request, res: Response) => {
